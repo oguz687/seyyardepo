8838be6b394308ff87e516e8dbaa10f70549f191
oguz687 <oguzhan_687@hotmail.com>
2020-04-09 00:32:38 +0300

    zds seyyar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialcompModule} from './materialcomp/materialcomp.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialcompModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
